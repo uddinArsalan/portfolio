2b6553a71808fd438fdef94f47b9b89885866f2f
Arsalan <uddinarsalan91@gmail.com>
2023-10-30 00:03:17 +0530

    Update Project Section

--- frontend/src/components/ProjectSection.tsx
+++ frontend/src/components/ProjectSection.tsx
@@ -9,12 +9,12 @@ const ProjectSection = () => {
         <div className="bg-gray-900 text-gray-300 flex flex-col items-center cursor-pointer " id="projects">
             <div className="text-[rgb(136,206,2)] text-5xl font-bold mt-8" >PROJECTS</div>
             <div className="flex flex-col md:w-3/4">
-                <div className="flex flex-col p-12">
+                <div className="flex flex-col md:p-12 p-9">
                     <img src={ThreadIt} alt="" className="" />
                     <a href="https://thread-it-one.vercel.app/" target="_blank"><div className="bg-[#0b0b0b] p-6 rounded-br-md rounded-bl-md flex justify-between">
                         <div>
                             <div><span className="font-bold">THREAD IT </span> </div>
-                            <div className="text-sm">Share hashnode blogs on twitter with ease in the form of threads</div>
+                            <div className="text-sm">Convert & Share Hashnode Blogs as Twitter Threads</div>
                         </div>
                         <i className="fa-solid fa-square-arrow-up-right text-[rgb(136,206,2)] text-4xl"></i>
                     </div></a>
@@ -24,7 +24,7 @@ const ProjectSection = () => {
                     <a href="https://sketchsync.netlify.app/" target="_blank"><div className="bg-[#0b0b0b] p-6 rounded-br-md rounded-bl-md flex justify-between">
                         <div>
                             <div><span className="font-bold">SKETCH SYNC</span> </div>
-                            <div className="text-sm">A Realtime Collaborative drawing app</div>
+                            <div className="text-sm">Collaborative Real-time Drawings & Creations</div>
                         </div>
                         <i className="fa-solid fa-square-arrow-up-right text-[rgb(136,206,2)] text-4xl"></i>
                     </div></a>
@@ -34,7 +34,7 @@ const ProjectSection = () => {
                     <a href="https://addictsy.netlify.app/" target="_blank"><div className="bg-[#0b0b0b] p-6 rounded-br-md rounded-bl-md flex justify-between">
                         <div className="flex flex-col">
                             <div><span className="font-bold">ADDICTSY</span> </div>
-                            <div className="text-sm">Share hashnode blogs on twitter with ease in the form of threads</div>
+                            <div className="text-sm">Empowering Addiction Recovery through Personal Chats</div>
                         </div>
                         <i className="fa-solid fa-square-arrow-up-right text-[rgb(136,206,2)] text-4xl"></i>
                     </div></a>
@@ -44,7 +44,7 @@ const ProjectSection = () => {
                     <a href="https://flixnest.netlify.app/" target="_blank"><div className="bg-[#0b0b0b] p-6 rounded-br-md rounded-bl-md flex justify-between">
                         <div className="flex flex-col">
                             <div><span className="font-bold">FLIX NEST</span> </div>
-                            <div className="text-sm">A Movie Website</div>
+                            <div className="text-sm">Explore & Bookmark Upcoming Movies and Trailers</div>
                         </div>
                         <i className="fa-solid fa-square-arrow-up-right text-[rgb(136,206,2)] text-4xl"></i>
                     </div></a>
@@ -54,7 +54,7 @@ const ProjectSection = () => {
                     <a href="https://beyondskill.netlify.app/" target="_blank"><div className="bg-[#0b0b0b] p-6 rounded-br-md rounded-bl-md flex justify-between">
                         <div className="flex flex-col">
                             <div><span className="font-bold">BEYOND SKILL</span> </div>
-                            <div className="text-sm">Share hashnode blogs on twitter with ease in the form of threads</div>
+                            <div className="text-sm">Unlock Your Potential with BeyondSkill's Wide Range of Courses</div>
                         </div>
                         <i className="fa-solid fa-square-arrow-up-right text-[rgb(136,206,2)] text-4xl"></i>
                     </div></a>
