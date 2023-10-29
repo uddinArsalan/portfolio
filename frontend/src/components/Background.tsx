e520f0b04e074954e178b688ece749d1584b0fc5
Arsalan <uddinarsalan91@gmail.com>
2023-10-29 22:35:33 +0530

    Updated social links

--- frontend/src/components/Background.tsx
+++ frontend/src/components/Background.tsx
@@ -7,7 +7,7 @@ const Background = () => {
   return (
     <>
       <header className="relative flex justify-center items-center text-white h-screen ">
-        <div className="mt-10 absolute grid grid-rows-3 place-items-center place-content-center justify-center z-20
+        <div className="absolute grid grid-row-3 gap-y-8 place-items-center place-content-center justify-center z-20
            ">
           <div className="font-bold text-6xl text-blue-700 text-center">WELCOME TO MY PORTFOLIO</div>
           <div className="bg-green-300 hover:bg-green-400 text-black font-semibold text-sm pt-2 pr-3 pb-2 pl-3 rounded-md w-32 flex justify-center items-center cursor-pointer">
@@ -23,9 +23,9 @@ const Background = () => {
             </Link>
           </div>
           <div className="flex gap-8 text-gray-500 text-5xl cursor-pointer">
-            <i className="fa-brands fa-square-x-twitter hover:text-white"></i>
-            <i className="fa-brands fa-github hover:text-gray-400"></i>
-            <i className="fa-brands fa-linkedin hover:text-blue-800"></i>
+            <a href="https://twitter.com/Arsalan_0101" target="_blank"><i className="fa-brands fa-square-x-twitter text-white"></i></a>
+            <a href="https://github.com/uddinArsalan" target="_blank"><i className="fa-brands fa-github text-gray-500"></i></a>
+            <a href="https://www.linkedin.com/in/arsalan-uddin-2356b81b9" target="_blank"><i className="fa-brands fa-linkedin text-blue-800"></i></a>
           </div>
         </div>
         <video
