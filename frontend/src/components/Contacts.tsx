a2f20eb8f0d54383f2d406d2fe2ce2295a856a96
Arsalan <uddinarsalan91@gmail.com>
2023-11-04 15:10:57 +0530

    Added Modal css

--- frontend/src/components/Contacts.tsx
+++ frontend/src/components/Contacts.tsx
@@ -1,4 +1,5 @@
 import ReCAPTCHA from "react-google-recaptcha";
+import { useState } from "react";
 import { useForm, ValidationError } from '@formspree/react';
 import Modal from './Modal';
 const Contacts = () => {
@@ -7,78 +8,83 @@ const Contacts = () => {
   //   return <p className="bg-black p-6 md:p-12 flex flex-col gap-12 md:gap-16 justify-center items-center"
   //   id="contact">Thanks for joining!</p>;
   // }
+  //flex-col gap-8 md:gap-16 p-8 md:p-12
+  const [open, setOpen] = useState<boolean>(true);
+
+  const closeModal = () => {
+    setOpen(prev => !prev)
+  }
   return (
-    <div
-      className="bg-black p-8 md:p-12 flex flex-col gap-12 md:gap-16 justify-center items-center"
-      id="contact"
-    >
-      {state.succeeded ?
-        <Modal />
-        :
-        <>
-          <div className="text-[rgb(136,206,2)] text-5xl font-bold">Contact Us</div>
-          <form
-            onSubmit={handleSubmit}
-            className="grid grid-rows-5 gap-8 md:gap-12 w-full"
-          >
-            <div className="">
-              <label
-                htmlFor="name"
-                className="text-[rgb(136,206,2)] text-4xl font-bold"
-              >
-                Name
-              </label>
-              <input id="name" type="text" name="name" className="p-8 w-full" placeholder='Enter your name' />
-              <ValidationError
-                prefix="Name"
-                field="name"
-                errors={state.errors}
-              />
-            </div>
-            <div>
-              <label
-                htmlFor="email"
-                className="text-[rgb(136,206,2)] text-4xl font-bold"
-              >
-                Email
-              </label>
-              <input id="email" type="email" name="email" className="p-8 w-full" placeholder='Enter your email' />
-              <ValidationError
-                prefix="Email"
-                field="email"
-                errors={state.errors}
-              />
-            </div>
-            <div>
-              <label
-                htmlFor="message"
-                className="text-[rgb(136,206,2)] text-4xl font-bold"
-              >
-                Project Idea
-              </label>
-              <textarea id="message"
-                name="message" className="p-8 w-full" placeholder='Enter your message here' />
-              <ValidationError
-                prefix="Message"
-                field="message"
-                errors={state.errors}
-              />
-            </div>
-              <ReCAPTCHA
-                sitekey="6LcnBu4oAAAAAJT2zNV3pVHXKIlL6haBuBE1h0sm"
-                className="items-start"
-              />
-            {/* <div className="g-recaptcha bg-white" data-sitekey="6LcnBu4oAAAAAJT2zNV3pVHXKIlL6haBuBE1h0sm">Hi Load Recaptcha</div> */}
-            <button
-              className="bg-[rgb(136,206,2)] rounded-md text-3xl font-bold"
-              type="submit" disabled={state.submitting}
+    <>
+        <div className={open || state.succeeded ? 'relative top-60' : 'hidden'}>
+          <Modal closeModal={closeModal} />
+        </div>
+      <div
+        className={`${open ? 'bg-gray-900 bg-opacity-50 blur-sm' : ''} ${"flex justify-center items-center md:gap-16 flex-col gap-8 bg-black p-8 md:p-12"} `}
+        id="contact"
+      >
+        <div className="text-[rgb(136,206,2)] text-5xl font-bold">Contact Us</div>
+        <form
+          onSubmit={handleSubmit}
+          className="grid grid-rows-5 gap-8 md:gap-12 w-full"
+        >
+          <div className="">
+            <label
+              htmlFor="name"
+              className="text-[rgb(136,206,2)] text-4xl font-bold"
             >
-              Submit
-            </button>
-          </form>
-        </>
-      }
-    </div>
+              Name
+            </label>
+            <input id="name" type="text" name="name" className="p-8 w-full" placeholder='Enter your name' />
+            <ValidationError
+              prefix="Name"
+              field="name"
+              errors={state.errors}
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="email"
+              className="text-[rgb(136,206,2)] text-4xl font-bold"
+            >
+              Email
+            </label>
+            <input id="email" type="email" name="email" className="p-8 w-full" placeholder='Enter your email' />
+            <ValidationError
+              prefix="Email"
+              field="email"
+              errors={state.errors}
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="message"
+              className="text-[rgb(136,206,2)] text-4xl font-bold"
+            >
+              Project Idea
+            </label>
+            <textarea id="message"
+              name="message" className="p-8 w-full" placeholder='Enter your message here' />
+            <ValidationError
+              prefix="Message"
+              field="message"
+              errors={state.errors}
+            />
+          </div>
+          <ReCAPTCHA
+            sitekey="6LcnBu4oAAAAAJT2zNV3pVHXKIlL6haBuBE1h0sm"
+            className="items-start"
+          />
+          {/* <div className="g-recaptcha bg-white" data-sitekey="6LcnBu4oAAAAAJT2zNV3pVHXKIlL6haBuBE1h0sm">Hi Load Recaptcha</div>  */}
+          <button
+            className="bg-[rgb(136,206,2)] rounded-md text-3xl font-bold"
+            type="submit" disabled={state.submitting}
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+    </>
   );
 };
 
--- frontend/src/components/Modal.tsx
+++ frontend/src/components/Modal.tsx
@@ -1,13 +1,15 @@
-const Modal = () => {
+interface propTypes {
+    closeModal : () => void
+}
+
+const Modal = ({ closeModal } : propTypes ) => {
     return (
-        <div className="">
-            <dialog open className="flex flex-col justify-around items-center p-10 bg-gray-800">
-                <p className="text-white text-2xl font-bold">Thanks for joining</p>
-                <form method="dialog">
-                    <button className="bg-[rgb(136,206,2)] rounded-md text-xl font-bold p-2 text-white">OK</button>
-                </form>
-            </dialog>
-        </div>
+        <dialog open className="grid grid-rows-2 place-items-center p-10 bg-gray-800 rounded-md z-20 w-1/2">
+            <p className="text-white text-xl font-semibold">Thanks for joining</p>
+            <form method="dialog">
+                <button className="bg-[rgb(136,206,2)] rounded-md text-lg font-bold p-2 text-white" onClick={closeModal}>OK</button>
+            </form>
+        </dialog>
     )
 }
 
