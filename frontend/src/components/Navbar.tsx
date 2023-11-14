e1b3d89b98c25d8de952c67469482a0ade635646
Arsalan <uddinarsalan91@gmail.com>
2023-11-14 15:21:33 +0530

    Some minor updates

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -32,7 +32,7 @@ const Navbar = ({ showNav, setShowNav }: NavbarProp) => {
         ></i>
       )}
       {showNav ? (
-        <div className="bg-[#151515] font-semibold text-2xl text-[#bebebe] grid grid-rows-5 md:grid-rows-6 grid-cols-1 place-items-center p-10 cursor-pointer md:h-screen lg:sticky lg:top-0 lg:bottom-0 lg:left-0 lg:col-span-1 col-span-2 navbar-transition">
+        <div className="bg-[#151515] font-semibold text-2xl text-[#bebebe] grid grid-rows-5 md:grid-rows-6 grid-cols-1 place-items-center p-10 cursor-pointer h-screen lg:sticky lg:top-0 lg:bottom-0 lg:left-0 lg:col-span-1 col-span-2 navbar-transition">
           <div className="flex justify-center items-center">
             <img
               src={portfolio}
