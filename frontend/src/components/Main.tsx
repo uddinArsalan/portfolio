72fbee428b01c26ece8d8fd666d4be6da4c2dff9
Arsalan <uddinarsalan91@gmail.com>
2023-11-26 21:40:35 +0530

    change the server link on client

--- frontend/src/components/Main.tsx
+++ frontend/src/components/Main.tsx
@@ -9,7 +9,7 @@ import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 //https://apollo-server-12hj.onrender.com
 
 const client = new ApolloClient({
-  uri: "http://localhost:4000/",
+  uri: "https://apollo-server-12hj.onrender.com",
   cache: new InMemoryCache(),
 });
 
