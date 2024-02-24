4365be07d77c3a2df662088cd1858c36261b1b21
Arsalan <uddinarsalan91@gmail.com>
2024-02-25 02:48:02 +0530

    Updated my skills

--- frontend/public/assets/skills svgs/index.ts
+++ frontend/public/assets/skills svgs/index.ts
@@ -1,28 +1,31 @@
-import react_svg from "./react.svg"
-import typescript_svg from "./typescript-icon.svg"
-import css_svg from "./css-3.svg"
-import graphql_svg from "./graphql.svg"
-import node_svg from "./nodejs.svg"
-import bootsrap_svg from "./bootstrap.svg"
-import javascript_svg from "./javascript.svg"
-import tailwindcss_svg from "./tailwindcss-icon.svg"
-import firebase_svg from "./firebase.svg"
-import html_svg from "./html-5.svg"
-import sql_svg from "./mysql.svg"
-import java_svg from "./java.svg"
-
+import react_svg from "./react.svg";
+import typescript_svg from "./typescript-icon.svg";
+import css_svg from "./css-3.svg";
+import graphql_svg from "./graphql.svg";
+import node_svg from "./nodejs.svg";
+import bootsrap_svg from "./bootstrap.svg";
+import javascript_svg from "./javascript.svg";
+import tailwindcss_svg from "./tailwindcss-icon.svg";
+import firebase_svg from "./firebase.svg";
+import html_svg from "./html-5.svg";
+import sql_svg from "./mysql.svg";
+import java_svg from "./java.svg";
+import nextjs_svg from "./nextjs-icon.svg";
+import material_ui_svg from "./material-ui.svg";
 
 export default {
-    bootsrap_svg,
-    tailwindcss_svg,
-    node_svg,
-    sql_svg,
-    react_svg,
-    javascript_svg,
-    typescript_svg,
-    graphql_svg,
-    css_svg,
-    firebase_svg,
-    java_svg,
-    html_svg
-}
+  nextjs_svg,
+  react_svg,
+  typescript_svg,
+  javascript_svg,
+  graphql_svg,
+  node_svg,
+  tailwindcss_svg,
+  sql_svg,
+  java_svg,
+  firebase_svg,
+  material_ui_svg,
+  bootsrap_svg,
+  css_svg,
+  html_svg,
+};
--- frontend/src/components/Skills.tsx
+++ frontend/src/components/Skills.tsx
@@ -2,12 +2,12 @@ import svgs from "../../public/assets/skills svgs/";
 
 const Skills = () => {
   return (
-    <div className="bg-hero bg-center bg-no-repeat bg-cover flex flex-col p-6 md:p-12 gap-6 items-center">
+    <div className=" bg-center bg-no-repeat bg-cover flex flex-col p-12 md:p-12 gap-12 items-center bg-zinc-950">
       <div className="text-[rgb(136,206,2)] text-4xl md:text-5xl font-bold">My Skills</div>
-      <div className="grid grid-cols-4 gap-6 md:gap-8 items-center mb-16">
+      <div className="grid grid-cols-4 md:grid-cols-5 gap-6 md:gap-8 items-center mb-16">
         {Object.keys(svgs).map((key) => (
           <div key={key}>
-            <img src={(svgs as any)[key]} alt=""  className="w-24 object-cover bg-cover p-[6px] md:p-4 bg-[rgb(0,0,0)] border-2 border-opacity-20 border-gray-200 rounded-lg hover:scale-105 cursor-pointer transition-transform flex justify-center items-center"/>
+            <img src={(svgs as any)[key]} alt=""  className="w-24 object-cover bg-cover p-[6px] md:p-4 bg-[rgb(0,0,0)] border-2 rounded-xl border-opacity-20 border-gray-200 hover:scale-105 cursor-pointer transition-transform flex justify-center items-center"/>
           </div>
         ))}
       </div>
